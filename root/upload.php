5e61ce7bc1233e07c8c4535e82a8c3c827b60af9
root <root@localhost.localdomain>
2019-05-22 11:49:06 +0300

    for Alim

--- root/upload.php
+++ root/upload.php
@@ -1,5 +1,32 @@
 <?php
+	session_start();
+	//mkdir("../")
+?>
+<!DOCTYPE html>
+<html lang="en" >
 
+<head>
+  	<meta charset="UTF-8">
+ 	<title>Cool site</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="script.js"></script>
+</head>
+
+<body>
+<table>
+<tr>
+<td width = "25%" valign="top">
+<form id="form1" action="upload.php" enctype="multipart/form-data" method="POST">
+		<div id = "div"> </div>
+		<button type="button" onclick="generateForm()">Add more</button>
+		<button type="submit">Upload</button>
+	</form>		
+	<script  src="js/index.js"></script>
+
+	
+</form>
+
+<?php
 // Проверим, успешно ли загружен файл
 //echo basename($_FILES['uploadfile']['tmp_name']);
 if (!empty($_FILES)){
@@ -23,7 +50,12 @@ if (!empty($_FILES)){
 		  }
 		$type = $_FILES['uploadfile']['type'];
 		$size = $_FILES['uploadfile']['size'];
-		//if (($type != "audio/mpeg") && ($type != "audio/wav")) exit;
+		/*
+		if (($type != "audio/mpeg") && ($type != "audio/wav")) {
+			echo "Unsupported file";
+			exit;
+		}
+		*/
 		//if ($size > 102400) exit; //размер не более 100кб
 
 		// Копируем файл из каталога для временного хранения файлов:
@@ -36,164 +68,76 @@ if (!empty($_FILES)){
 			echo "<h3>Upload error (2)!</h3> Error type: ".$errors['type']. ". Message: " .$errors['message'];
 			exit;
 		}
-		echo "<BR>";
-		echo $uploadfile;
-		echo "<br>";
-		echo $uploadfile_name;
-		echo "<br>";
-		$audio_info = sounds_info($uploadfile_name);
-		for($j = 0; $j < 6; $j++) {
-			echo round($audio_info[$j], 2);
-			echo "<br>";
-		}
-		echo "<br>";
-		/*$audio_probs = sounds_classify($uploadfile_name);
-		$audio_tags = array('accordion', 'piano', 'violin', 'guitar', 'noise');
-		echo "probabilities: <br>";
-		for ($i = 0; $i < 5; $i++) {
-			echo $audio_tags[$i];
-			echo " ";
-			echo $audio_probs[$i];
-			echo "<br>";
-		}
-		echo sounds_speed('alltta_bucket.wav', 'whew.wav', 1.5);
-		echo "<br>";
-		echo "<br>";
-		$arr_test = sounds_test(5);
-		echo $arr_test[0];
-		echo "<br>";
-		echo $arr_test[1];
-		echo "<br>";
-		echo $arr_test[2];
-		echo "<br>";*/
+		
 	}
 }
 
-
-/*
-echo "<BR>";
-//Вставить код обработки файла
-echo "ArcType: ".$_POST['ArcType']."<BR>";
-switch($_POST['ArcType']) {
-	case 0:
-		echo "0";
-		break;
-	case 1:
-		echo "1";
-		break;
-	case 2:
-		echo "2";
-		break;
-	case 3:
-		echo "3";
-		break;
-	case 4:
-		echo "4";
-		break;
-}
-/*exec('ls -l', $output);
-foreach ($output as $item) echo $item."<BR>";
-
-echo "<BR>";
-echo "<A href=" . $uploadfile . ">Download file</A>";
-*/
-
-/*
-if($_GET['btn_f1']){send_file('function_1');}
-
-if($_GET['btn_f2']){send_file('function_2');}
-
-
-
-function send_file($option){
-	$out = [];	
-	$radios = document.getElementsByName('file_radio');
-	for($i = 0; $i < $radios.length; $i++){
-		if($radios[$i].checked){
-			$fname = $radios[i].id;
-			$out = [$fname, $option];
-			
-			echo '<script language="javascript">';
-			echo 'alert("('.$out[0].', '.$out[1].')")';
-			echo '</script>';	
-		}
-	}
-}
-
-function weird_flex(){
-	echo $_POST['text_vol'];
-	}
-*/
-
-
-?>
-<body>
-<?php
-
-if(isset($_POST['btn_vol'])){
-	increase_volume();	
-}
-
-function increase_volume(){
-	$file = $_POST['file_radio'];
-	$file_dir = 'Sounds/'.$file;
-	$uploadfile = './Sounds/changed_'.$file;
-	$k = (double)$_POST['text_vol'];
-	sounds_volume($file_dir, $uploadfile, $k);
-	
-	//echo "<A href=" . $uploadfile . ">Download file</A>";
-}
-
-if(isset($_POST['btn_spd'])){
-	increase_speed();
-}
-
-function increase_speed(){
-	$file = $_POST['file_radio'];
-	$filename = strtok($file, '.');
-	$extension = strtok('.');
-	$mult = (double)$_POST['text_spd'];
-
-	sounds_speed('Sounds/'.$file, 'Sounds/'.$filename.'(1).'.$extension, $mult);
-}
-
-if(isset($_POST['btn_cut'])){
-	crop();
-}
-
-function crop(){
-	$file = $_POST['file_radio'];
-	$filename = strtok($file, '.');
-	$extension = strtok('.');
-	$l_border = (int)$_POST['text_cut_left'];
-	$r_border = (int)$_POST['text_cut_right'];
-	sounds_crop('Sounds/'.$file, 'Sounds/'.$filename.'_cutted.'.$extension, $l_border, $r_border);
-}
-
-if(isset($_POST['btn_rename'])){
-	rename_file();
-}
-
-function rename_file(){
-	$file = $_POST['file_radio'];
-	//$filename = substr(strrchr($file, "/"), 1);
-	$newname = '../Audios/Archive/Sounds/'.$_POST['text_rename'].'.wav';
-	rename('../Audios/Archive/Sounds/'.$file, $newname);	
-}
-
-if(isset($_POST['btn_del'])){
-	delete_file();
-}
-
-function delete_file(){
-	$file = $_POST['file_radio'];
-	unlink('../Audios/Archive/Sounds/'.$file) or die("Error while deleting");
-}
 ?>
 
 
+</td>
+<td valign="top">
 <form action="" enctype="multipart/form-data" method="POST">
+	<?php
 
+	if(isset($_POST['btn_vol'])){
+		increase_volume();	
+	}
+
+	function increase_volume(){
+		$file = $_POST['file_radio'];
+		$file_dir = 'Sounds/'.$file;
+		$uploadfile = './Sounds/changed_'.$file;
+		$k = (double)$_POST['text_vol'];
+		sounds_volume($file_dir, $uploadfile, $k);
+	}
+
+	if(isset($_POST['btn_spd'])){
+		increase_speed();
+	}
+
+	function increase_speed(){
+		$file = $_POST['file_radio'];
+		$filename = strtok($file, '.');
+		$extension = strtok('.');
+		$mult = (double)$_POST['text_spd'];
+
+		sounds_speed('Sounds/'.$file, 'Sounds/'.$filename.'(1).'.$extension, $mult);
+	}
+
+	if(isset($_POST['btn_cut'])){
+		crop();
+	}
+
+	function crop(){
+		$file = $_POST['file_radio'];
+		$filename = strtok($file, '.');
+		$extension = strtok('.');
+		$l_border = (int)$_POST['text_cut_left'];
+		$r_border = (int)$_POST['text_cut_right'];
+		sounds_crop('Sounds/'.$file, 'Sounds/'.$filename.'_cutted.'.$extension, $l_border, $r_border);
+	}
+
+	if(isset($_POST['btn_rename'])){
+		rename_file();
+	}
+
+	function rename_file(){
+		$file = $_POST['file_radio'];
+		//$filename = substr(strrchr($file, "/"), 1);
+		$newname = '../Audios/Archive/Sounds/'.$_POST['text_rename'].'.wav';
+		rename('../Audios/Archive/Sounds/'.$file, $newname);	
+	}
+
+	if(isset($_POST['btn_del'])){
+		delete_file();
+	}
+
+	function delete_file(){
+		$file = $_POST['file_radio'];
+		unlink('../Audios/Archive/Sounds/'.$file);//or die("Error while deleting");
+	}
+	?>
 	<?php
 	
 	$current_dir = '../Audios/Archive/Sounds/';
@@ -201,7 +145,7 @@ function delete_file(){
 	$dir = opendir($current_dir);
 
 	//echo "<p>Каталог загрузки: $current_dir</p>";
-	echo '<p>Your files:</p><ul>';
+	echo 'Your files:<ul>';
 	while ($file = readdir($dir)) {
 		if($file != '.' && $file  != '..' ) {
 			echo "<ul> <input type='radio' id='$file' name='file_radio' value=$file> $file </ul>";
@@ -212,13 +156,6 @@ function delete_file(){
 	echo '</ul>';
 	closedir($dir);
 	
-	/*
-	for($i=0; $i<count($upload_files); $i++){
-		echo "<li>$upload_files[$i] <input type='radio' id='$upload_files[$i]' name='file_radio' value='$upload_files[$i]'></li>";
-		//echo "<li>$upload_files[0] <input type='radio' id='$upload_files[0]' name='file_radio' value='$upload_files[0]'></li>";
-		//echo count($upload_files);
-	}
-	*/
 	?>
 	<p>
 	<button id="btn_clsfy" name="btn_clsfy">Classify</button>
@@ -229,16 +166,17 @@ function delete_file(){
 	<?php
 	function classify(){
 		$path = 'Sounds/'.$_POST['file_radio'];
-		//$probs = sounds_classify($path);
-		
-		return $path;
+		$probs = sounds_classify($path);
+		return $probs;
 	}
 	if(isset($_POST['btn_clsfy'])){
-		//$prob= classify();
-		echo $prob;
-		$instruments = array('accordion', 'piano', 'violin', 'guitar', 'smth else');
+		$probs = classify();
+		if($probs[0]==-1) {
+			echo "classify error!";
+		}
+		$instruments = array('accordion', 'piano', 'violin', 'guitar', 'flute');
 		for($i=0; $i<5; $i++){
-			echo "<script>GenerateDiv('$instruments[$i]', '228');</script>";
+			echo "<script>GenerateDiv('$instruments[$i]', '');</script>";
 		}
 		
 	} 
@@ -277,6 +215,11 @@ function delete_file(){
 	
 
 </form>
+</td>
+</tr>
+</table>
+</body>
+</html>
 
 	
 
